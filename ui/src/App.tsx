4374f76f8afef5148a0a7f113b9e4d5a280e8255
kaylem williams <kaylemw1@outlook.com>
2025-03-30 14:57:19 +1300

    add title

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     if (comboBoxValue == 2) {
       suffix = "t"
     }
-    else if(comboBoxValue == 3){
+    else if (comboBoxValue == 3) {
       suffix = "d"
     }
     return `1/${fractionValues[value]}${suffix}`
@@ -39,35 +39,44 @@ function App() {
 
   return (
     <main className="h-screen w-screen flex items-center justify-center flex-col">
-      <div className="h-fit flex items-center justify-evenly gap-4 flex-col">
-        <Slider
-          identifier="dryWet"
-          controlParameterIndexAnnotation={controlParameterIndexAnnotation}
-          displayWithUnit={convertValueToPercentage}
-          key="dryWet"
-        />
-        <Slider
-          identifier="feedback"
-          controlParameterIndexAnnotation={controlParameterIndexAnnotation}
-          displayWithUnit={convertValueToPercentage}
-          key="feedback"
-        />
-        {comboBoxValue == 0 ? (<Slider
-          identifier="delayTime"
-          controlParameterIndexAnnotation={controlParameterIndexAnnotation}
-          displayWithUnit={convertValueToSeconds}
-        />) : (
+      <div className="divider"></div>
+      <div className="flex justify-center items-center w-full">
+        <div style={{
+          writingMode: "vertical-rl",
+          fontSize: "9vw"
+        }} className="text-primary-content grid place-content-center font-black">
+          DELAY
+        </div>
+        <div className="h-fit flex items-center justify-evenly gap-4 flex-col">
           <Slider
-            identifier="delayTimeFraction"
+            identifier="dryWet"
             controlParameterIndexAnnotation={controlParameterIndexAnnotation}
-            displayWithUnit={convertValueToFraction}
+            displayWithUnit={convertValueToPercentage}
+            key="dryWet"
           />
-        )}
-        <ComboBox
-          value={comboBoxValue}
-          setValue={setComboBoxValue}
-          identifier="delayTimeType"
-          controlParameterIndexAnnotation={controlParameterIndexAnnotation} />
+          <Slider
+            identifier="feedback"
+            controlParameterIndexAnnotation={controlParameterIndexAnnotation}
+            displayWithUnit={convertValueToPercentage}
+            key="feedback"
+          />
+          {comboBoxValue == 0 ? (<Slider
+            identifier="delayTime"
+            controlParameterIndexAnnotation={controlParameterIndexAnnotation}
+            displayWithUnit={convertValueToSeconds}
+          />) : (
+            <Slider
+              identifier="delayTimeFraction"
+              controlParameterIndexAnnotation={controlParameterIndexAnnotation}
+              displayWithUnit={convertValueToFraction}
+            />
+          )}
+          <ComboBox
+            value={comboBoxValue}
+            setValue={setComboBoxValue}
+            identifier="delayTimeType"
+            controlParameterIndexAnnotation={controlParameterIndexAnnotation} />
+        </div>
       </div>
       <div className="divider"></div>
       Made By Kaylem
